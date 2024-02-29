c38759c6e1be37e964c22dc171046b7e0313e4af
Loris <lorispuchol@gmail.com>
2024-02-29 21:01:55 +0100

    je sais pas

--- p1/scripts/install-k3s-server.sh
+++ p1/scripts/install-k3s-server.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 curl -sfL https://get.k3s.io | sh - < /dev/null
-sudo cp -f /var/lib/rancher/k3s/server/node-token >> /vagrant/confs/
-
+sudo mkdir -p /vagrant/credentials/
+sudo cp -f /var/lib/rancher/k3s/server/node-token /vagrant/credentials/
