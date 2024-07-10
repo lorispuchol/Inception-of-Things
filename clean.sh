664d739832079fa6cf89ad74351389b8d2519436
lorispuchol <lorispuchol@gmail.com>
2024-07-10 21:56:33 +0200

    var vagrant file

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
 vagrant destroy -f
-vagrant box remove debian/bookworm64
+vagrant box remove debian/bookworm64 bento/debian-12
+vagrant box remove bento/debian-12
 rm -rf .vagrant credentials
 echo Done
